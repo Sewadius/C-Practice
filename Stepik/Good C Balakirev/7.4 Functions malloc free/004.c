22848b1d057bb2a2ba58cae33cc0b8d7027196e9
Sewadius <vertnather@gmail.com>
2024-09-05 15:20:13 +0300

    added to 7.4 chapter

--- Stepik/Good C Balakirev/7.4 Functions malloc free/004.c
+++ Stepik/Good C Balakirev/7.4 Functions malloc free/004.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TOTAL 4
 
 int main(void)
 {
-    
+    short* data = malloc(TOTAL * sizeof(short));
+    short values[TOTAL] = {-4, 3, 0, 100};
+    short* ptr_data = data;
+
+    for (int i = 0; i < TOTAL; ++i) {
+        *ptr_data = values[i];
+        ++ptr_data;
+    }
+
+    //__ASSERT_TESTS__
+    free(data);
     return 0;
 }
