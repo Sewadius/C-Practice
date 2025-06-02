c22ea208ffc750c1bfa52ba98d8d6c2c750ed87a
Sewadius <vertnather@gmail.com>
2025-06-02 12:24:18 +0300

    new file in 3.1 chapter

--- Stepik/Basics of C programming. Tasks/1.2 First C program/004.c
+++ Stepik/Basics of C programming. Tasks/1.2 First C program/004.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #define ROW 49
 #define LINES 13
+#define STARS 8
 
-int main() 
+int main()
 {
-    for (int i = 0; i < ROW; ++i) {
+    for (unsigned char i = 0; i < LINES; ++i)
+    {
+        if (0 < i && i < 7)
+        {
+            for (unsigned char k = 0; k < STARS; ++k)
+                printf("*");
+            for (unsigned char l = STARS; l < ROW; ++l)
+                printf("_");
+        }
+        else
+        {
+            for (unsigned char j = 0; j < ROW; ++j)
+                printf("_");
+        }
 
+        printf("\n");
     }
 
     return 0;
