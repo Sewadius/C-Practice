164935850239898edf13b59362da0d8b68675c7d
Sewadius <vertnather@gmail.com>
2026-01-13 20:50:14 +0300

    added new Shieldt book

---
+++ Books/Shildt_tutorial_3ed/001_BriefOverview/001_first.cpp
@@ -0,0 +1,9 @@
+// Page 16 - программа C++ в современном стиле
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+
---
+++ Books/Shildt_tutorial_3ed/001_BriefOverview/003_userInput.cpp
@@ -0,0 +1,14 @@
+// Page 22 - пользователь вводит целое число
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+
+    cout << "Введите число: ";
+    cin >> i;
+    cout << "Вот ваше число: " << i << '\n';
+
+    return 0;
+}
+
