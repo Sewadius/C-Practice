f281bc70162dc481b60a2816797f1f33e3eee7e9
Sewadius <vertnather@gmail.com>
2026-01-20 15:23:00 +0300

    new files in chapter 3

---
+++ Books/Prata_6_ed/003_WorkData/011_divide.cpp
@@ -0,0 +1,21 @@
+// Page 118 - деление целых чисел и с плавающей точкой
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+
+    cout << "Integer division: 9/5 = " << 9 / 5 << endl;
+    cout << "Floating-point division: 9.0/5.0 = ";
+    cout << 9.0 / 5.0 << endl;
+
+    cout << "Mixed division: 9.0/5 = " << 9.0 / 5 << endl;
+    cout << "Double constants: 1e7/9.0 = ";
+    cout << 1.e7 / 9.0 << endl;
+
+    cout << "Float constants: 1e7f/9.0f = ";
+    cout << 1.e7f / 9.0f << endl;
+
+    return 0;
+}
+
