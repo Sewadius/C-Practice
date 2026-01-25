7dc4dc2e47b5344f0116719687126e25e227ae78
Sewadius <vertnather@gmail.com>
2026-01-25 19:43:02 +0300

    added string progs chapter 3

---
+++ Books/Prata_6_ed/004_ComposeTypes/006_numstr.cpp
@@ -0,0 +1,20 @@
+// Page 145 - смешивание числового и строкового ввода
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "What year was your house built?\n";
+    int year;
+    (cin >> year).get();    // Чтобы прочитать и отбросить '\n'
+
+    cout << "What is its street address?\n";
+    char address[80];
+    cin.getline(address, 80);
+
+    cout << "Year built: " << year << endl;
+    cout << "Address: " << address << endl;
+    cout << "Done!\n";
+
+    return 0;
+}
+
