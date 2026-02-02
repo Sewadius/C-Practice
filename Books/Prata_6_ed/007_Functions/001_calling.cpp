bc2401a777a31eda014fb209e18025e2aa8a05bd
Sewadius <vertnather@gmail.com>
2026-02-02 17:10:38 +0300

    added 1 file to chapter 7

---
+++ Books/Prata_6_ed/007_Functions/001_calling.cpp
@@ -0,0 +1,18 @@
+// Page 304 - определение, прототип и вызов функции
+#include <iostream>
+using namespace std;
+
+void simple();
+
+int main() {
+    cout << "main() will call the simple() function:\n";
+    simple();
+    cout << "main() is finished with the simple() function.\n";
+
+    return 0;
+}
+
+void simple() {
+    cout << "I'm but a simple function.\n";
+}
+
